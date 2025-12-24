c84a71535e0f551c6fd9dd5d319115418c089ab4
jonasklvn1234@gmail.com <jonasklvn1234@gmail.com>
2025-12-24 13:32:00 -0300

    criação do componente Cadastro e Consulta

---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FlexLayoutModule, MatCardModule],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss',
+})
+export class Cadastro {
+
+}
