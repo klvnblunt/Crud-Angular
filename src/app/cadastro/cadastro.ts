d065aa2175b9ee571ac7c94a64acdbaafa1dd1cd
jonasklvn1234@gmail.com <jonasklvn1234@gmail.com>
2025-12-26 12:01:27 -0300

    Criação do formulario de cadastro e da classe Usuario

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatCardModule } from '@angular/material/card'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente'
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule],
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
 export class Cadastro {
 
+    cliente: Cliente = Cliente.newCliente()
+
+    salvar(){
+      console.log("Dados clientes: ", this.cliente)
+    }
 }
+
+
