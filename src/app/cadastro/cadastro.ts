255887e2600041b11cf53c29db99c43c83a2b00f
jonasklvn1234@gmail.com <jonasklvn1234@gmail.com>
2025-12-26 12:24:01 -0300

    Criação do Service para a parte logica da aplicação

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -7,6 +7,7 @@ import { MatInputModule } from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
 import { Cliente } from './cliente'
+import { ClienteService } from '../cliente.service'
 
 @Component({
   selector: 'app-cadastro',
@@ -18,8 +19,12 @@ export class Cadastro {
 
     cliente: Cliente = Cliente.newCliente()
 
+  constructor(private service: ClienteService){
+
+  }
+
     salvar(){
-      console.log("Dados clientes: ", this.cliente)
+      this.service.salvar(this.cliente)
     }
 }
 
